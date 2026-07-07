f788b9782eee67067c08eb315bf36796d493edf9
dhgmlrud00 <dhgmlrud1215@gmail.com>
2026-07-07 14:52:34 +0900

    퀵메뉴 및 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./css/home.css";
 import BottomNav from "./components/BottomNav";
 import Header from "./components/Header";
 import Mainbanner from "./components/Mainbanner";
+import QuickMenu from "./components/QuickMenu";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Mainbanner />
+      <QuickMenu />
       <BottomNav />
     </div>
   );
---
+++ src/components/QuickMenu.jsx
@@ -0,0 +1,32 @@
+function QuickMenu() {
+  const menus = [
+    { image: "/images/quick_intro.png", title: "포인핸드\n소개"},
+    {image: "/images/quick_campaign.png", title: "입양\n캠페인"},
+    { image: "/images/quick_donation.png", title: "포인기부"},
+    { image: "/images/quick_benefit.png", title: "지원/혜택"},
+    { image: "/images/quick_store.png", title: "포인핸드\n스토어"},
+    { image: "/images/quick_sponsor.png", title: "포인핸드\n후원" },
+  ];
+
+  return (
+   <section>
+    <h2>퀵메뉴</h2>
+
+    <div className="quick-list">
+    {menus.map((menu, index) => (
+        <button key={index} className="quick-item">
+        <span className="quick-icon">
+            <img src={menu.image} alt={menu.title} />
+        </span>
+
+        <span className="quick-name">
+            {menu.title}
+        </span>
+        </button>
+    ))}
+    </div>
+   </section>
+  );
+}
+
+export default QuickMenu;
