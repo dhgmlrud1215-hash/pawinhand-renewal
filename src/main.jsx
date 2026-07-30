5a6c2c54b5ad3f5cc129df04c66330d1a417d171
dhgmlrud00 <dhgmlrud1215@gmail.com>
2026-07-30 17:30:05 +0900

    상단여백

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
 
+if (window.self !== window.top) {
+  document.documentElement.classList.add("is-embedded");
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
